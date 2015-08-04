0494850958c60a534cc0de43ceaa7df47e9824c3
cwp10 <cwp10@msn.com>
2015-08-04 11:23:52 +0900

    Start
    Start

---
+++ Assets/Scripts/NetworkHealth.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+[NetworkSettings (channel = 1)]
+public class NetworkHealth : NetworkBehaviour {
+
+	[Range(0, 100)]
+	public float playerHealth = 100.0f;
+	private NetworkPlayer netPlayer;
+
+	void Awake() {
+		
+		netPlayer = GetComponent<NetworkPlayer>();
+		netPlayer.eventResetPlayer += OnResetHealth;
+	}
+
+	public override void OnNetworkDestroy () {
+		netPlayer.eventResetPlayer -= OnResetHealth;
+	}
+
+	public void TakeDamage(float dmg) {
+		playerHealth -= dmg;
+		RpcUpdateHealth(playerHealth);
+	}
+
+	[ClientRpc(channel = 1)]
+	private void RpcUpdateHealth(float health) {
+		playerHealth = health;
+
+		if(playerHealth <= 0.0f) {
+			netPlayer.PlayerDead();
+		}
+	}
+
+	public void OnResetHealth() {
+		playerHealth = 100;
+	}
+}
