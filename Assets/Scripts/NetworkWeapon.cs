ded20d227311d6829bc9146b5172a1a07d08d945
cwp10 <cwp10@msn.com>
2015-08-04 13:20:47 +0900

    Revert "Revert "Start""
    This reverts commit 8f8e4ba28915259c05387424ab3326198d9d2e78.

---
+++ Assets/Scripts/NetworkWeapon.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+[NetworkSettings (channel = 1)]
+public class NetworkWeapon : NetworkBehaviour {
+	
+	public float maxBulletDist = 100;
+	public float wallParticleTime = 2;
+	public float bloodParticleTime = 2;
+
+	public Transform firstPersonCharacter;
+	public GameObject wallParticlePrefab;
+	public GameObject playerParticlePrefab;
+
+
+	public float weaponRate = 0.2f;
+	public float damage = 20.0f;
+
+	private float fireRate;
+	private NetworkPlayer netPlayer;
+	
+	void Start() {
+		
+		netPlayer = GetComponent<NetworkPlayer>();
+	}
+	
+	[ClientCallback]
+	void Update() {
+
+		if(Input.GetButton("Fire1") && isLocalPlayer && netPlayer.anim.GetCurrentAnimatorStateInfo(1).normalizedTime > 0.95f) {
+			fireRate += Time.deltaTime;
+
+			if(fireRate >= weaponRate) {
+
+				var ray = new Ray(firstPersonCharacter.position, firstPersonCharacter.forward);
+				var hit = new RaycastHit();
+				
+				if(Physics.Raycast(ray, out hit, maxBulletDist)) {
+					var tag = hit.transform.tag;
+					
+					switch(tag) {
+					case "Player":
+						CmdInvokeParticle(tag, hit.point, hit.normal);
+						NetworkInstanceId id = hit.transform.GetComponent<NetworkIdentity>().netId;
+						CmdShoot(id);
+						break;
+					default:
+						CmdInvokeParticle(tag, hit.point, hit.normal);
+						break;
+					}
+				}
+				netPlayer.anim.SetTrigger("Shoot");
+				fireRate = 0.0f;
+			}
+		}
+
+		if(Input.GetKeyDown(KeyCode.R) && isLocalPlayer && netPlayer.anim.GetCurrentAnimatorStateInfo(1).normalizedTime > 0.95f) {
+			netPlayer.anim.SetTrigger("Reload");
+			fireRate = 0.0f;
+		}
+	}
+
+	[Command(channel = 1)]
+	private void CmdShoot(NetworkInstanceId id) {
+		GameObject player = NetworkServer.FindLocalObject(id);
+		var healthScript = player.GetComponent<NetworkHealth>();
+		if(healthScript == null) {
+			Debug.LogError("no healthScripts attached to player");
+			return;
+		} 
+		healthScript.TakeDamage(damage);
+	}
+
+	[Command(channel = 1)]
+	private void CmdInvokeParticle(string tag, Vector3 pos, Vector3 normal) {
+		RpcInvokeParticle(tag, pos, normal);
+	}
+
+	[ClientRpc(channel = 1)]
+	private void RpcInvokeParticle(string tag, Vector3 pos, Vector3 normal) {
+
+		GameObject prefab;
+		float time;
+
+		switch(tag) {
+		case "Player":
+			prefab = playerParticlePrefab;
+			time = bloodParticleTime;
+			break;
+		default:
+			prefab = wallParticlePrefab;
+			time = wallParticleTime;
+			break;
+		}
+
+		ShowParticles(prefab, pos, normal, time);
+	}
+
+	public void ShowParticles(GameObject prefab, Vector3 pos, Vector3 normal, float time) {
+		var particles = Instantiate(prefab, pos, Quaternion.LookRotation(normal));
+		Destroy(particles, time);
+	}
+}
---
+++ Assets/Scripts/ObjectShake.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectShake : MonoBehaviour {
+	
+	private Transform tr;
+	private Vector3 originPosition;
+	
+	private bool isShaking;
+	
+	void Start()
+	{
+		tr = this.GetComponent<Transform>();
+	}
+	
+	bool IsShake()
+	{
+		return isShaking;
+	}
+	
+	public void Shake(float duration = 0.5f, float delay = 0f, float _magnitude = 0.1f)
+	{
+		if (isShaking)
+			return;
+		
+		ShakeStop();
+		StartCoroutine(this.PlayShake(delay, duration, _magnitude));
+	}
+	
+	IEnumerator PlayShake(float _delayTime, float _duration, float _magnitude)
+	{
+		isShaking = true;
+		originPosition = tr.localPosition;
+		
+		yield return new WaitForSeconds(_delayTime);
+		
+		float elapsed = 0.0f;
+		float duration = _duration;
+		float magnitude = _magnitude;
+		
+		while (elapsed < duration)
+		{
+			elapsed += Time.deltaTime;
+			float percentComplete = elapsed / duration;
+			float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+			// map noise to [-1, 1]
+			float x = Random.value * 2.0f - 1.0f;
+			float y = Random.value * 2.0f - 1.0f;
+			
+			x *= magnitude * damper;
+			y *= magnitude * damper;
+			
+			Vector3 newPos = new Vector3(originPosition.x + x, originPosition.y + y, originPosition.z);
+			tr.localPosition = newPos;
+			
+			yield return null;
+		}
+		
+		tr.localPosition = originPosition;
+		isShaking = false;
+	}
+	
+	public void ShakeStop()
+	{
+		StopAllCoroutines();
+		isShaking = false;
+	}
+}
